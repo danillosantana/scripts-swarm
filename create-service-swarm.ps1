405b2e8c97649b3deb49706e5aa89912cd98b42b
Danillo Santana <dsantana@stj.jus.br>
2021-01-13 23:05:36 -0300

    Scripts para Automatizar o Processo de Criação Imagens e Servicços

---
+++ create-service-swarm.ps1
@@ -0,0 +1,23 @@
+﻿param([string] $PASTA_PROJETO, 
+        [string] $PASTA_AMBIENTE, [string] $TIPO_AMBIENTE, 
+        [int32]$PORT, [string] $PROJETO)
+
+
+
+$buscarService = docker service ls -f "name=$PROJETO-$TIPO_AMBIENTE" -q
+
+if ( $buscarService -eq $null )
+{
+    $info = '[INFO] -----Criando Serviço '
+    $info += $PROJETO+"-"+$TIPO_AMBIENTE
+    $info += '--------------------------'    
+    Write-Output $info
+    docker service create --name=$PROJETO-$TIPO_AMBIENTE  $PROJETO-$TIPO_AMBIENTE
+    docker service update --publish-add published=$PORT,target=8087 $PROJETO-$TIPO_AMBIENTE
+} else {
+      $info = '[INFO] -----Atualizando Serviço '
+      $info += $PROJETO+"-"+$TIPO_AMBIENTE
+      $info += '--------------------------'    
+    Write-Output $info  
+      docker service update $PROJETO-$TIPO_AMBIENTE
+}
